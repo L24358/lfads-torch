5e64ba7ab308ce4675b38d727fc457b630090658
L24358 <belleliu@uw.edu>
2023-11-16 00:16:08 +0000

    Commit before cloning forked repository.

---
+++ regression_model/model.py
@@ -0,0 +1,14 @@
+import hydra
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from torch import nn
+
+class MRRM(pl.LightningModule):
+    """
+    Multi-Regional Regression Model.
+    """
+    def __init__(
+        self,
+    ):
+        pass
